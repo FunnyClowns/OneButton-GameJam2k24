3ebde96c29f9cbbd44b228bd82ced9de58fdd86a
FunnyClowns <faradeaken@gmail.com>
2024-12-12 20:55:54 +0700

    added reset position option in moveondrag class

--- Assets/Scripts/Input/MoveOnDrag.cs
+++ Assets/Scripts/Input/MoveOnDrag.cs
@@ -4,26 +4,42 @@ using UnityEngine.InputSystem;
 
 public class MoveOnDrag : MonoBehaviour, IMouse {
 
+    [Header("Optional Settings")]
+    [Space(10)]
+    [SerializeField] bool resetPositionAtMouseUP;
+    Vector2 startPosition;
+
     [Space(10)]
     [Tooltip("Optional call, when mouse is lifted")]
     [SerializeField] UnityEvent inputCancelledCall;
 
+    [HideInInspector] public bool isDragged;
     Vector2 difference;
+
+    void Start(){
+        if (resetPositionAtMouseUP)
+            startPosition = this.transform.position;
+    }
 
     public void OnMouseClick(InputAction.CallbackContext context){
         // Debug.Log("click");
 
+        isDragged = true;
         difference = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
     }
 
     public void OnMouseGrab(InputAction.CallbackContext context){
-        Vector2 mouseVelocity = context.ReadValue<Vector2>();
 
         transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - difference;
         // Debug.Log("Mouse vel = " + mouseVelocity * 1);
     }
 
     public void OnMouseStop(InputAction.CallbackContext context){
+        isDragged = false;
+
         inputCancelledCall.Invoke();
+        
+        if (resetPositionAtMouseUP)
+            this.transform.position = startPosition;
     }
 }
