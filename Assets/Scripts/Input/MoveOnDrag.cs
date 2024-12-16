fc5d42c4656bdf99ba642ced28d25d3cb51ff14c
FunnyClowns <faradeaken@gmail.com>
2024-12-16 21:31:54 +0700

    feat : restrict drag move pos to screen

--- Assets/Scripts/Input/MoveOnDrag.cs
+++ Assets/Scripts/Input/MoveOnDrag.cs
@@ -1,12 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class MoveOnDrag : MonoBehaviour, IMouse {
 
+    Vector2 extents;
+
+    Vector2 topRight;
+    Vector2 bottomLeft;
+    const float CAMERA_LIMIT_OFFSET = 1.5f;
+
     [Header("Optional Settings")]
     [Space(10)]
     [SerializeField] bool resetPositionAtMouseUP;
+    [SerializeField] bool isMoveRestrictedToScreen;
     Vector2 startPosition;
 
     [Space(10)]
@@ -19,6 +27,18 @@ public class MoveOnDrag : MonoBehaviour, IMouse {
     void Start(){
         if (resetPositionAtMouseUP)
             startPosition = this.transform.position;
+
+        if (isMoveRestrictedToScreen){
+            topRight = Camera.main.ViewportToWorldPoint(Vector3.one);
+            bottomLeft = Camera.main.ViewportToWorldPoint(Vector3.zero);
+
+            topRight.y *= CAMERA_LIMIT_OFFSET;
+            bottomLeft.y *= CAMERA_LIMIT_OFFSET;
+
+            extents = GetComponent<SpriteRenderer>().sprite.bounds.extents;
+
+            // Debug.Log("Extents : " + extents);
+        }
     }
 
     public void OnMouseClick(InputAction.CallbackContext context){
@@ -30,8 +50,15 @@ public class MoveOnDrag : MonoBehaviour, IMouse {
 
     public void OnMouseGrab(InputAction.CallbackContext context){
 
-        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - difference;
-        // Debug.Log("Mouse vel = " + mouseVelocity * 1);
+        Vector2 pos = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - difference;
+
+        if (isMoveRestrictedToScreen){
+            pos.x = Mathf.Clamp(pos.x, bottomLeft.x + extents.x, topRight.x - extents.x);
+            pos.y = Mathf.Clamp(pos.y, bottomLeft.y + extents.y, topRight.y - extents.y);
+        }
+
+        transform.position = pos;
+        // Debug.Log("Pos = " + pos);
     }
 
     public void OnMouseStop(InputAction.CallbackContext context){
